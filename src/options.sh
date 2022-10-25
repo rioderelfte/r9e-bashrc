0c01b4fd71d9eb09b06d1087904cf64606a2da1d
Florian Sowade <f.sowade@suora.com>
2022-10-25 23:54:34 +0200

    Add some fzf configuration

--- src/options.sh
+++ src/options.sh
@@ -25,8 +25,13 @@ if dircolors --sh >/dev/zero 2>&1; then
 fi
 
 export BAT_THEME="OneHalfLight"
+export FZF_DEFAULT_OPTS='--color=fg:-1,bg:-1,preview-fg:-1,preview-bg:-1,hl:196,fg+:-1,bg+:254,hl+:196,gutter:-1,pointer:9'
 if _r9e_is_executable 'fd'; then
     export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
+    export FZF_CTRL_T_COMMAND='fd --type f --type d --hidden --exclude .git'
+fi
+if _r9e_is_executable 'bat' && _r9e_is_executable 'exa'; then
+    export FZF_CTRL_T_OPTS='--preview "test -d {} && exa -lF --icons --group-directories-first {} || bat -f {}"'
 fi
 
 if _r9e_is_executable brew; then
